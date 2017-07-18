725bae355ae182a147639814fc2f731749a200d2
Lincoln Junior <lincoln.silva.junior@gmail.com>
2017-07-18 01:40:37 -0300

    Alteração no grid do bootstrap para otimizar o modo responsivo do sidebar

--- wp-content/themes/reservas/sidebar.php
+++ wp-content/themes/reservas/sidebar.php
@@ -6,7 +6,7 @@
  * @package Reservas
  */
 ?>
-<div id="secondary" class="col-md-3 sidebar widget-area" role="complementary">
+<div id="secondary" class="col-xs-10 col-xs-offset-1 col-sm-6 col-sm-offset-3 col-md-3 col-md-offset-0 sidebar widget-area" role="complementary">
     <?php do_action( 'before_sidebar' ); ?>
    <?php dynamic_sidebar( 'sidebar-1' ); ?>
 </div><!-- #secondary .widget-area -->
